06784b211e508531938672490d89c7e3e7ef05d9
KaeroDot <KaeroDot@contact.me.on.git.hub>
2017-06-30 22:21:42 +0200

    add: infogetmatrix, infosetmatrix and added to selftest

--- selftest_scripts_octave.m
+++ selftest_scripts_octave.m
@@ -1,6 +1,17 @@
 % run tests implemented in function definitions:
 addpath('GNU_Octave_scripts')
+
 test infosetsection.m
 test infogetsection.m
+
 test infosettext.m
 test infogettext.m
+
+test infosetnumber.m
+test infogetnumber.m
+
+test infosettime.m
+test infogettime.m
+
+test infosetmatrix.m
+test infogetmatrix.m
