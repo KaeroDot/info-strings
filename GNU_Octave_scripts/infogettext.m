42af97ffffcd48e9daf93d3149aa6170f87a70c7
KaeroDot <KaeroDot@contact.me.on.git.hub>
2017-06-30 10:19:34 +0200

    fix: quotation mark in example

--- GNU_Octave_scripts/infogettext.m
+++ GNU_Octave_scripts/infogettext.m
@@ -11,7 +11,7 @@
 ##
 ## Example:
 ## @example
-## infostr = "A:: 1\nsome note\nB([V?*.])::    !$^&*()[];::,.\n#startmatrix:: simple matrix \n1;  2; 3; \n4;5;         6;  \n#endmatrix:: simple matrix \nC:: c without section\n#startsection:: section 1 \n  C:: c in section 1 \n  #startsection:: subsection\n    C:: c in subsection\n  #endsection:: subsection\n#endsection:: section 1\n#startsection:: section 2\n  C:: c in section 2\n#endsection:: section 2\n"
+## infostr = sprintf('A:: 1\nsome note\nB([V?*.])::    !$^&*()[];::,.\n#startmatrix:: simple matrix \n1;  2; 3; \n4;5;         6;  \n#endmatrix:: simple matrix \nC:: c without section\n#startsection:: section 1 \n  C:: c in section 1 \n  #startsection:: subsection\n    C:: c in subsection\n  #endsection:: subsection\n#endsection:: section 1\n#startsection:: section 2\n  C:: c in section 2\n#endsection:: section 2\n')
 ## infogettext(infostr,'A')
 ## infogettext(infostr,'B([V?*.])')
 ## infogettext(infostr,'C')
