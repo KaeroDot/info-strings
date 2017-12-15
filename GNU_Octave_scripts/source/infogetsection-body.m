b11de98a1d538376d802ed5b92bc642bb0dff8a7
KaeroDot <KaeroDot@contact.me.on.git.hub>
2017-12-15 10:42:03 +0100

    add: octave: infogetsection use get_section.m

--- GNU_Octave_scripts/source/infogetsection-body.m
+++ GNU_Octave_scripts/source/infogetsection-body.m
@@ -35,52 +35,12 @@ function [section, endposition] = infogetsection(varargin) %<<<1
                 error('infogetsection: scell must be a cell of strings')
         endif
 
-        % format inputs %<<<2
+        % prepare inputs %<<<2
         if ~isempty(key)
                 scell = [scell {key}];
         endif
 
         % get section %<<<2
-        section = '';
-        endposition = 0;
-        while (~isempty(infostr))
-                % search sections one by one from start of infostr to end
-                [S, E, TE, M, T, NM] = regexpi(infostr, ['#startsection\s*::\s*(.*)\s*\n(.*)\n\s*#endsection\s*::\s*\1'], 'once');
-                if isempty(T)
-                        % no section found
-                        section = '';
-                        break
-                else
-                        % some section found
-                        if strcmp(strtrim(T{1}), scell{1})
-                                % wanted section found
-                                section = strtrim(T{2});
-                                endposition = endposition + TE(end,end);
-                                break
-                        else
-                                % found section is not the one wanted
-                                if E < 2
-                                        % danger of infinite loop! this should never happen
-                                        error('infogetsection: infinite loop happened!')
-                                endif
-                                % remove previous parts of infostr to start looking for 
-                                % wanted section after the end of found section:
-                                infostr = infostr(E+1:end);
-                                % calculate correct position that will be returned to user:
-                                endposition = endposition + E;
-                        endif
-                endif
-        endwhile
-        % if nothing found:
-        if isempty(section)
-                error(['infogetsection: section `' scell{1} '` not found'])
-        endif
-        % some result was obtained. if subsections are required, do recursion:
-        if length(scell) > 1
-                % recursively call for subsections:
-                [tmpsection, tmppos] = infogetsection(section, scell(2:end));
-                endposition = endposition - (length(section) - tmppos);
-                section = tmpsection;
-        endif
+        section = get_section('infogetsection', infostr, scell)
 endfunction
 
