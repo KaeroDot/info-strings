fd0691b9890f3dd2f6491fc374b7ff5d7a390e60
KaeroDot <KaeroDot@contact.me.on.git.hub>
2019-05-15 21:12:23 +0200

    fix: make_regexp working also in matlab version of regexps

--- GNU_Octave_scripts/source/make_regexp.m
+++ GNU_Octave_scripts/source/make_regexp.m
@@ -3,31 +3,55 @@ function expr = make_regexp(key, type)
         % this function is mainly to have all regular expressions at one place to increase
         % readability of the whole library.
 
+        % GNU Octave and LabVIEW use PCRE (Pearl Compatible Regular Expressions), however Matlab
+        % seems to have it's own regular expression flavor (unfortunately).
+
+
         %remove leading spaces of key and escape characters:
         key = regexpescape(strtrim(key));
 
         % (?m) is regexp flag causing that ^ and $ match start and end of line, however $ do not match end of file
 
         if strcmpi(type, 'linekey')
-                % SOL SP key SP :: content without EOL
-                % (?-s) is regexp flag causing that . do not match end of line
-                expr = ['(?m-s)^\h*' key '\h*::(.*)'];
-                % results: Token 1: content of key
+                if isOctave
+                        % SOL SP key SP :: content without EOL
+                        % (?-s) is regexp flag causing that . do not match end of line
+                        expr = ['(?m-s)^\h*' key '\h*::(.*)'];
+                        % results: Token 1: content of key
+                else
+                        % \h has to be replaced by \s. But \s matches also newlines contrary to \h
+                        expr = ['(?m-s)^\s*' key '\s*::(.*)'];
+                endif
         elseif strcmpi(type, 'matrix')
-                % SOL SP startmatrix SP :: SP key SP ( NL content NL OR NL ) SP endmatrix SP :: SP key SP EOL
-                % This is NOT greedy, i.e. finds first occurance of endmatrix.
-                expr = ['(?m)^\h*#startmatrix\h*::\h*' key '\h*(?:\n(.*?)\n|\n)^\h*#endmatrix\h*::\h*' key '\h*$'];
-                % results: Token 1: content of section OR token does not exist
+                if isOctave
+                        % SOL SP startmatrix SP :: SP key SP ( NL content NL OR NL ) SP endmatrix SP :: SP key SP EOL
+                        % This is NOT greedy, i.e. finds first occurance of endmatrix.
+                        expr = ['(?m)^\h*#startmatrix\h*::\h*' key '\h*(?:\n(.*?)\n|\n)^\h*#endmatrix\h*::\h*' key '\h*$'];
+                        % results: Token 1: content of section OR token does not exist
+                else
+                        % \h has to be replaced by \s. But \s matches also newlines contrary to \h
+                        expr = ['(?m)^\s*#startmatrix\s*::\s*' key '\s*(?:\n(.*?)\n|\n)^\s*#endmatrix\s*::\s*' key '\s*$'];
+                endif
         elseif strcmpi(type, 'section')
-                % SOL SP startsection SP :: SP key SP ( NL content NL OR NL ) SP endsection SP :: SP key SP EOL
-                expr = ['(?ms)^\h*#startsection\h*::\h*' key '\h*(?:\n(.*)\n|\n)^\h*#endsection\h*::\h*' key '\h*$'];
-                % results: Token 1: content of section OR token does not exist
+                if isOctave
+                        % SOL SP startsection SP :: SP key SP ( NL content NL OR NL ) SP endsection SP :: SP key SP EOL
+                        expr = ['(?ms)^\h*#startsection\h*::\h*' key '\h*(?:\n(.*)\n|\n)^\h*#endsection\h*::\h*' key '\h*$'];
+                        % results: Token 1: content of section OR token does not exist
+                else
+                        % \h has to be replaced by \s. But \s matches also newlines contrary to \h
+                        expr = ['(?ms)^\s*#startsection\s*::\s*' key '\s*(?:\n(.*)\n|\n)^\s*#endsection\s*::\s*' key '\s*$'];
+                endif
         elseif strcmpi(type, 'anysection')
-                % SOL SP startsection SP :: SP key SP ( NL content NL OR NL ) SP endsection SP :: SP key SP EOL
-                % This is greedy, i.e. finds last occurance of endsection.
-                expr = ['(?ms)^\h*#startsection\h*::\h*(.*)\h*(?:\n(.*)\n|\n)^\h*#endsection\h*::\h*\1\h*$'];
-                % results: Token 1: key
-                % results: Token 2: content of section OR token does not exist
+                if isOctave
+                        % SOL SP startsection SP :: SP key SP ( NL content NL OR NL ) SP endsection SP :: SP key SP EOL
+                        % This is greedy, i.e. finds last occurance of endsection.
+                        expr = ['(?ms)^\h*#startsection\h*::\h*(.*)\h*(?:\n(.*)\n|\n)^\h*#endsection\h*::\h*\1\h*$'];
+                        % results: Token 1: key
+                        % results: Token 2: content of section OR token does not exist
+                else
+                        % \h has to be replaced by \s. But \s matches also newlines contrary to \h
+                        expr = ['(?ms)^\s*#startsection\s*::\s*(.*)\s*(?:\n(.*)\n|\n)^\s*#endsection\s*::\s*\1\s*$'];
+                endif
         else
                 error('unknown regular expression type')
         endif
